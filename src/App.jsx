a49e3986e888beceae254bebc1d5268161289ee1
skvorzof <skvorzof@ya.ru>
2021-04-26 20:48:30 +0300

    ✏️ refact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useStore from './store';
 
-import { Input } from './components';
+import { List, Input } from './components';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,30 +18,11 @@ function App() {
       <div className="cols">
         <div className="col">
           <h2>Вода</h2>
-          <ul>
-            {water.map((item) => {
-              let d = new Date(item.date).toDateString('ru');
-              return (
-                <li key={item.date}>
-                  {item.value} <span className="date">{d}</span>
-                </li>
-              );
-            })}
-          </ul>
+          <List data={water} />
         </div>
         <div className="col">
           <h2>Энергия</h2>
-          <ul>
-            {energy.map((item) => {
-              let d = new Date(item.date).toDateString('ru');
-              return (
-                <li key={item.date}>
-                  {item.value}
-                  <span className="date">{d}</span>
-                </li>
-              );
-            })}
-          </ul>
+          <List data={energy} />
         </div>
       </div>
     </div>
