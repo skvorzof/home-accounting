d0b18806b4415a21b8462b3409d8f7c7a82cbefe
Mitrofan ogly <4594651+skvorzof@users.noreply.github.com>
2021-06-02 20:52:47 +0300

    reverse List view

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 export const List = (props) => {
   const { data } = props;
+  console.log(data.reverse());
   return (
     <ul>
-      {data.map((item) => {
-        // Форматируем дату из Timestamp
-        let date = new Intl.DateTimeFormat('ru-RU').format(item.date);
-        return (
-          <li key={item.date}>
-            {item.value} <span className="date"> - {date}</span>
-          </li>
-        );
-      })}
+      {data
+        .map((item) => {
+          // Форматируем дату из Timestamp
+          let date = new Intl.DateTimeFormat('ru-RU').format(item.date);
+          return (
+            <li key={item.date}>
+              {item.value} <span className="date"> - {date}</span>
+            </li>
+          );
+        })
+        .reverse()}
     </ul>
   );
 };
